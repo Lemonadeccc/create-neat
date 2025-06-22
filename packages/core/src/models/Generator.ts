ee9f010711ce82674c88aa3a297a06c7b66e6c54
黄开浩 <2134084703@qq.com>
2025-06-22 23:10:04 +0800

    fix: 简化构建工具初始配置复杂度 (#324)

--- packages/core/src/models/Generator.ts
+++ packages/core/src/models/Generator.ts
@@ -337,8 +337,12 @@ class Generator {
 
     // 与构建工具有关的配置全部添加完毕，生成构建工具配置文件
     const buildConfigFinalContent = generator.default(this.buildToolConfigAst).code;
-    // 将构建工具配置文件也添加到根文件树对象中
-    const buildToolConfigName = `${this.buildTool}.config.js`;
+
+    // 根据是否是TypeScript项目决定配置文件扩展名
+    const isTypeScript = process.env.isTs === "true";
+    const fileExtension = isTypeScript ? "ts" : "js";
+    const buildToolConfigName = `${this.buildTool}.config.${fileExtension}`;
+
     this.files.addToTreeByFile(buildToolConfigName, buildConfigFinalContent);
 
     // 从package.json中生成额外的的文件(如果extraConfigFiles为true时需要)
